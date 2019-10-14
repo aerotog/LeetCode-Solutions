54f007020a5657ffa7f3d60edd8de5b784c51c38
Tim Guenthner <aerotog@gmail.com>
2019-10-13 20:53:34 -0500

    Add solution for pancake sort

--- solutions/medium/969_pancake_sort.py
+++ solutions/medium/969_pancake_sort.py
@@ -3,25 +3,32 @@ from typing import List
 
 
 def pancakeSort(A: List[int]) -> List[int]:
-    # Get sorted list, big to small
-    # For each in sorted, do moves to get to last position
-    # Flip 0..i where i is biggest
-    # Flip len(A)
-    N = len(A)
-    B = sorted(range(1, N+1), key=lambda i: -A[i-1])
-    # B = sorted(range(1, N + 1), key=lambda i: -A[i - 1])
+    # This can be solved by putting biggest number at the end one at a time
+    # Flip up to next biggest number to get to front
+    # Flip up to desired position of next biggest number
+    # The original position of each number will be changed by each flip
+
+    length = len(A)
+    # Get original positions of numbers biggest to smallest
+    original_positions = sorted(range(1, length + 1), key=lambda i: -A[i - 1])
+
+    flips = []
+
+    for position in original_positions:
+        for flip in flips:
+            # Original position moves with each existing flip if included in flip
+            if position <= flip:
+                # Original position moved by single flip
+                position = flip + 1 - position
+
+        # Next moves will be current position of next biggest, then desired position of next biggest
+        flips.extend([position, length])
+        # The next desired position will be directly before the last insertion
+        length -= 1
+
+    return flips
 
 
+A = [3, 2, 4, 1]
 
-    return B
-
-
-
-
-
-ip = [3,2,4,1]
-
-#
-
-print(pancakeSort(ip))
-
+print(pancakeSort(A))
